d1ca30ff439a3a70c4df655e4a30fc1af37bf0c9
octavionotpunk <83534113+Octave-byte@users.noreply.github.com>
2022-07-02 11:02:39 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0x23581767a106ae21c074b2276D25e5C3e136a68b",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "My NFT Collection",
+    "title": "Moonbirds dashboard",
 
     // 4. (Optional) Display the floor price chart
     "timeseries_chart": true,
 
     // 5. (Optional) Set your banner image
-    "banner_picture": "",
+    "banner_picture": "https://img.seadn.io/files/785e4dff3024e87ae60cf695e4f0f0cb.png?fit=max&w=600",
   },
 
   // Supported networks - just for your reference
